7a9abfc4cb5e6e2fe9ab5a12281a8f5883eeb3e7
lele-maxwell <leleivanlele22@gmail.com>
2025-01-14 10:22:10 +0100

    created a directory and added docker file, mock files, and tha the automated backup script

---
+++ mock-data/backup-data.sh
@@ -0,0 +1,17 @@
+SOURCE_DIR="/app/mock-data"          
+BACKUP_DIR="/app/backup"            
+TIMESTAMP=$(date +"%Y%m%d_%H%M%S")   
+ARCHIVE_NAME="backup_$TIMESTAMP.tar.gz"
+
+mkdir -p "$BACKUP_DIR"
+
+tar -czf "$BACKUP_DIR/$ARCHIVE_NAME" -C "$(dirname "$SOURCE_DIR")" "$(basename "$SOURCE_DIR")"
+if [[ $? -eq 0 ]]; then
+  
+    echo "Backup created successfully: $BACKUP_DIR/$ARCHIVE_NAME"
+    else
+    echo "Backup failed."
+    exit 1
+fi
+
+
