14d6d02bb246453fada4a651cb8ae731a6549ad2
Leghadjeu Christian <legochristianarthur@gmail.com>
2025-01-14 20:40:05 +0100

    Update recovery_script.sh with variables

--- mock-data/recovery_script.sh
+++ mock-data/recovery_script.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+ SOURCE_DIR="/app/mock-data"          
+ BACKUP_DIR="/app/backup"            
+ TIMESTAMP=$(date +"%Y%m%d_%H%M%S")   
+ ARCHIVE_NAME="backup_$TIMESTAMP.tar.gz"
 
 # defining the DIR variable that contains the original directory location of our mock data files
-export DIR=$(dirname $SOURCE_DIR)
+ DIR=$(dirname $SOURCE_DIR)
 
 tar -xzf $BACKUP_DIR/$ARCHIVE_NAME -C $DIR 
